97f223b8b8586ddde07b957e8e2ae7229e09698b
ShaneRandell <77622184+ShaneRandell@users.noreply.github.com>
2021-04-05 15:42:15 -0300

    Update Lab-12.4.py

--- Lab-12.4.py
+++ Lab-12.4.py
@@ -1,6 +1,6 @@
 import cv2 as cv
 import numpy as np
-img = cv.imread(‘Foto1.jpg’)
+img = cv.imread(‘hockey.jpeg’)
 res = cv.resize(img,None,fx=0.5, fy=0.5, interpolation = cv.INTER_CUBIC)
 cv.imwrite("canny.jpg", cv.Canny(res, 200, 300)) # Canny in one line!
 cv.imshow("canny", cv.imread("canny.jpg"))
